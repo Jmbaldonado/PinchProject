f07f8d91af208ef725a8eef33be3354a362ff06f
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-06-29 16:42:04 +0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by John Michael Baldonado on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
