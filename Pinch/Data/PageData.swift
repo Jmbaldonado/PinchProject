77da43939205faff7e6de1c72c327c58483eeb72
John Michael Baldonado <jm.baldonado@whitecloak.com>
2022-06-29 19:57:08 +0800

    Pinch Project SwiftUI MasterClass 2022 - IOS App Development & Swift

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by John Michael Baldonado on 6/29/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
